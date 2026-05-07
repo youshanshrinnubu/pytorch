9f1ff8beb23ecaca9181284efd73c88991183333
Guilherme Leobas <gleobas@quansight.com>
2026-05-07 00:08:28 +0000

    [Dynamo] Implement nb_add  / nb_inplace_add
    ghstack-source-id: 481b40ccc82d53f97d0a095fda8bc5cd100d256e
    Pull-Request: https://github.com/pytorch/pytorch/pull/182234

--- torch/_dynamo/variables/base.py
+++ torch/_dynamo/variables/base.py
@@ -780,6 +780,15 @@ class VariableTracker(metaclass=VariableTrackerMeta):
             from .object_protocol import generic_hash
 
             return generic_hash(tx, self)
+        elif name == "__add__":
+            # ref: https://github.com/python/cpython/blob/3.13/Objects/typeobject.c#L10231-L10233
+            #      https://github.com/python/cpython/blob/3.13/Objects/typeobject.c#L8551-L8561
+            return self.nb_add_impl(tx, args[0])
+        elif name == "__radd__":
+            # ref: https://github.com/python/cpython/blob/3.13/Objects/typeobject.c#L8563-L8573
+            return self.nb_add_impl(tx, args[0], reverse=True)
+        elif name == "__iadd__":
+            return self.nb_inplace_add_impl(tx, args[0])
         elif name == "__sub__":
             # ref: https://github.com/python/cpython/blob/3.13/Objects/typeobject.c#L10231-L10233
             #      https://github.com/python/cpython/blob/3.13/Objects/typeobject.c#L8551-L8561
@@ -1239,6 +1248,27 @@ class VariableTracker(metaclass=VariableTrackerMeta):
             "the corresponding VariableTracker doesn't implement nb_negative_impl.",
             hints=[*graph_break_hints.SUPPORTABLE],
         )
+
+    def nb_add_impl(
+        self,
+        tx: Any,
+        other: VariableTracker,
+        reverse: bool = False,
+    ) -> VariableTracker:
+        """tp_as_number->nb_add slot. Default: graph break.
+
+        ``reverse=True`` means self is the right-hand operand (CPython would
+        look up ``__radd__`` instead of ``__add__``).
+        """
+        return self._nb_slot_not_implemented("nb_add_impl", other, reverse=reverse)
+
+    def nb_inplace_add_impl(
+        self,
+        tx: Any,
+        other: VariableTracker,
+    ) -> VariableTracker:
+        """tp_as_number->nb_inplace_add slot. Default: graph break."""
+        return self._nb_slot_not_implemented("nb_inplace_add", other)
 
     def nb_subtract_impl(
         self,
