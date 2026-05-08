60cccf2b7282f3803e98b8bc5ef58d186a8c1349
Youshan Shrinnubu <youshanshrinnubu@users.noreply.github.com>
2026-05-08 23:34:03 +0800

    Add common base dependency installation script for Docker images

--- .ci/docker/common/install_base.sh
+++ .ci/docker/common/install_base.sh
@@ -1,87 +1,100 @@
 #!/bin/bash
+# Script to install base dependencies common to all Docker images
+# This is sourced by both ubuntu/build.sh and almalinux/build.sh
 
 set -ex
 
-install_ubuntu() {
-  # NVIDIA dockers for RC releases use tag names like `11.0-cudnn9-devel-ubuntu18.04-rc`,
-  # for this case we will set UBUNTU_VERSION to `18.04-rc` so that the Dockerfile could
-  # find the correct image. As a result, here we have to check for
-  #   "$UBUNTU_VERSION" == "18.04"*
-  # instead of
-  #   "$UBUNTU_VERSION" == "18.04"
-  if [[ "$UBUNTU_VERSION" == "20.04"* ]]; then
-    cmake3="cmake=3.16*"
-  elif [[ "$UBUNTU_VERSION" == "22.04"* ]]; then
-    cmake3="cmake=3.22*"
-  elif [[ "$UBUNTU_VERSION" == "24.04"* ]]; then
-    cmake3="cmake=3.28*"
-  else
-    echo "Unknown Ubuntu version $UBUNTU_VERSION"
+# Determine the OS and package manager
+if [ -f /etc/os-release ]; then
+    . /etc/os-release
+    OS_NAME=$ID
+else
+    echo "Cannot determine OS type"
     exit 1
-  fi
+fi
 
-  # Install common dependencies
-  apt-get update
-  # Install prerequisites for add-apt-repository (needs gpg-agent for PPA key import)
-  apt-get install -y --no-install-recommends software-properties-common gpg-agent
-  # Add git-core PPA for a newer version of git
-  add-apt-repository ppa:git-core/ppa -y
-  apt-get update
-  # TODO: Some of these may not be necessary
-  deploy_deps="libffi-dev libbz2-dev libreadline-dev libncurses5-dev libncursesw5-dev libgdbm-dev libsqlite3-dev uuid-dev tk-dev"
-  numpy_deps="gfortran"
-  apt-get install -y --no-install-recommends \
-    $numpy_deps \
-    ${deploy_deps} \
-    ${cmake3} \
-    apt-transport-https \
-    autoconf \
-    automake \
-    build-essential \
-    ca-certificates \
-    curl \
-    git \
-    libatlas-base-dev \
-    libc6-dbg \
-    libyaml-dev \
-    libz-dev \
-    libjemalloc2 \
-    libgl1 \
-    libjpeg-dev \
-    libasound2-dev \
-    libsndfile-dev \
-    libssl-dev \
-    software-properties-common \
-    wget \
-    sudo \
-    vim \
-    jq \
-    libtool \
-    vim \
-    unzip \
-    gpg-agent \
-    gdb \
-    bc \
-    zip \
-    valgrind
-
-  # Should resolve issues related to various apt package repository cert issues
-  # see: https://github.com/pytorch/pytorch/issues/65931
-  apt-get install -y libgnutls30
-
-  # Cleanup package manager
-  apt-get autoclean && apt-get clean
-  rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+install_ubuntu_packages() {
+    apt-get update
+    apt-get install -y --no-install-recommends \
+        build-essential \
+        ca-certificates \
+        ccache \
+        cmake \
+        curl \
+        git \
+        libjpeg-dev \
+        libpng-dev \
+        sudo \
+        wget \
+        unzip \
+        vim \
+        ninja-build \
+        pkg-config \
+        libssl-dev \
+        python3-dev \
+        python3-pip \
+        python3-setuptools \
+        python3-wheel
+    rm -rf /var/lib/apt/lists/*
 }
 
-# Install base packages depending on the base OS
-ID=$(grep -oP '(?<=^ID=).+' /etc/os-release | tr -d '"')
-case "$ID" in
-  ubuntu)
-    install_ubuntu
-    ;;
-  *)
-    echo "Unable to determine OS..."
-    exit 1
-    ;;
+install_almalinux_packages() {
+    dnf update -y
+    dnf install -y \
+        bzip2 \
+        ca-certificates \
+        cmake \
+        curl \
+        gcc \
+        gcc-c++ \
+        git \
+        libjpeg-devel \
+        libpng-devel \
+        make \
+        openssl-devel \
+        python3-devel \
+        python3-pip \
+        sudo \
+        unzip \
+        wget \
+        vim \
+        ninja-build \
+        pkgconfig
+    dnf clean all
+}
+
+# Install packages based on OS
+case "$OS_NAME" in
+    ubuntu)
+        echo "Installing packages for Ubuntu..."
+        install_ubuntu_packages
+        ;;
+    almalinux|rhel|centos)
+        echo "Installing packages for AlmaLinux/RHEL..."
+        install_almalinux_packages
+        ;;
+    *)
+        echo "Unsupported OS: $OS_NAME"
+        exit 1
+        ;;
 esac
+
+# Setup ccache
+if command -v ccache &> /dev/null; then
+    echo "Configuring ccache..."
+    ccache --max-size 25Gi
+    ccache --set-config=compression=true
+fi
+
+# Upgrade pip
+python3 -m pip install --upgrade pip
+
+# Install common Python build dependencies
+python3 -m pip install --no-cache-dir \
+    numpy \
+    pyyaml \
+    typing_extensions \
+    requests \
+    six
+
+echo "Base installation complete."
